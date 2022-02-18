1bb0ecd3d563f7792bc04ca49562784d51aacc8b
miq28 <iqbal.tiro@gmail.com>
2022-02-18 14:03:02 +0700

    improve error handling

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,56 +4,52 @@ const jwt = require("jsonwebtoken");
 const HTTP_STATUS_CODE = {
     OK: 200,
     BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
     FORBIDDEN: 403,
     NOT_FOUND: 404,
-    INTERNAL_SERVER: 500
-}
-
-const ERROR = {
-    FORBIDDEN: 'Forbidden',
-    NOT_FOUND: 'Not Found',
-    NOT_ALLOWED: 'Not Allowed',
-    JSONWEBTOKEN: 'Jwt Error',
-    PRISMA: 'Prisma Error'
+    INTERNAL_SERVER_ERROR: 500
 }
 
 function logErrors(err, req, res, next) {
-    console.error(err.stack)
+    console.error(err)
     next(err)
 }
 
 function clientErrorHandler(err, req, res, next) {
-    err.stack = err.stack || ''
     let status = err.status || 500
     let code = err.code || 'Unknown'
-    let message = err.message || "Unknown error"
-    if (message.includes(ERROR.FORBIDDEN)) {
+    let message = err.message || "Internal server error"
+    if (message.toLowerCase().includes('forbidden')) {
         status = HTTP_STATUS_CODE.FORBIDDEN
     }
-    if (message.includes("found")) {
-        status = HTTP_STATUS_CODE.INTERNAL_SERVER
+    if (message.toLowerCase().includes("found")) {
+        status = HTTP_STATUS_CODE.NOT_FOUND
     }
-    if (err instanceof jwt.JsonWebTokenError) {
-        status = HTTP_STATUS_CODE.FORBIDDEN
-        message = 'JsonWebTokenError. ' + message
+    if (err.name === 'TokenExpiredError') {
+        status = HTTP_STATUS_CODE.UNAUTHORIZED
+        message = err.message
+    }
+    if (err.name === 'JsonWebTokenError') {
+        status = HTTP_STATUS_CODE.UNAUTHORIZED
+        message = err.message
     }
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        code = ERROR.PRISMA
+        status = HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR
+        code = err.code
+        message = err.message
     }
 
-    // 
-
     res.status(status)
-    res.json({ status, code, message })
+    res.send({ status, code, message })
 }
 
 function errorHandler(err, req, res, next) {
+    console.log('ERROR 3')
     res.status(500)
-    res.render('error', { error: err })
+    res.json({ error: err })
 }
 
 module.exports = {
-    ERROR,
     HTTP_STATUS_CODE,
     logErrors,
     clientErrorHandler,
