dda488391f80e18cad4dedff92dea9e24389ecb9
miq28 <iqbal.tiro@gmail.com>
2022-02-20 19:28:06 +0700

    improve modify path util (photo can also be nested under album)

--- middleware/modifyImagePath.js
+++ middleware/modifyImagePath.js
@@ -21,6 +21,30 @@ function modifyImagePath(req,res,next) {
     next()
 }
 
+function modifyImagePath2ndLayer(req,res,next) {
+    const resultObj = req.result.photos
+    // check if the incoming data is array, e.g. from prisma.getMany
+    if(Array.isArray(resultObj) && resultObj.length > 0){
+        // console.log('is Array')
+        for ( let i = 0; i < resultObj.length; i++){            
+            const modifiedPath = `${req.protocol}://${req.headers.host}/${resultObj[i].path}`
+            resultObj[i].path = modifiedPath
+        }
+    }
+    // If not an array, then it must be an object.
+    // Check for empty object
+    else if (Object.keys(resultObj).length > 0){
+        // console.log('resultObj must be an Object')
+        const modifiedPath = `${req.protocol}://${req.headers.host}/${resultObj.path}`
+        resultObj.path = modifiedPath
+    }
+
+    //  here the path inside resultObj has been modified
+    req.result = resultObj
+    next()
+}
+
 module.exports ={
-    modifyImagePath
+    modifyImagePath,
+    modifyImagePath2ndLayer
 }
