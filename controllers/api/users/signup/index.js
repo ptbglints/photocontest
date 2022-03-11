8f37e2c18908e03b55aa0479d5d040d1a4ecbc5d
miq28 <92211442+miq28@users.noreply.github.com>
2022-03-12 00:24:44 +0700

    fix bugs

--- controllers/api/users/signup/index.js
+++ controllers/api/users/signup/index.js
@@ -29,9 +29,9 @@ const signup = async (req, res, next) => {
     try {
         let { userName, email, password } = req.body
 
-        if (userName !== 'maksi') {
-            throw new Error ('Registration blocked! Only username maksi is allowed.')
-        }       
+        // if (userName !== 'maksi') {
+        //     throw new Error ('Registration blocked! Only username maksi is allowed.')
+        // }       
 
         const encryptedPassword = await EncriptPassword(password)
 
